47e064782708fa87da8964c9b053c325f8c61674
sachindukandekumbura <sachindukandekumbura1@gmail.com>
2026-08-02 01:39:00 -0700

    Create StatusUpdateRequest

---
+++ backend/src/main/java/com/borrowbuddy/backend/dto/StatusUpdateRequest.java
@@ -0,0 +1,17 @@
+package com.borrowbuddy.backend.dto;
+
+public class StatusUpdateRequest {
+
+    private String status;
+
+    public StatusUpdateRequest() {
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
